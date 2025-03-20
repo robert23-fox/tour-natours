5f15e7824d448bc1480746d8c644a16388b096be
robert23-fox <caiarobertalexandru@gmail.com>
2025-03-20 12:30:19 +0200

    Fix dependencies

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
+const cors = require('cors');
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
@@ -19,11 +20,18 @@ const compression = require("compression")
 
 const app = express();
 
+app.enable('trust proxy');
+
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
 // 1) GLOBAL MIDDLEWARES
 // Serving static files
+app.use(cors());
+
+app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors());
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/node_modules", express.static(path.join(__dirname, "node_modules")));
 
